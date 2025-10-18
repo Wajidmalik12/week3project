796ed2a649c3f74b0f7326c0a5503e69284ee301
Wajidmalik12 <mawking123456@gmail.com>
2025-10-18 08:55:09 -0700

    Add files via upload

---
+++ week3/pro3.py
@@ -0,0 +1,28 @@
+sen = input("enter any sentence : ")
+print(sen.__len__())
+print(sen.upper())
+print(sen[0])
+print(sen[-1])
+
+#2
+def story():
+    name = input("Enter any name : ")
+    place = input("enter any place : ")
+    obj = input("Enter any object : ")
+    feeling = input("Enter any feelings : ")
+    print(name+" is  student " + place + " is his home town " + obj + " is his fav things and he is feeling "+ feeling)
+
+story()
+
+phr = input("enter any phrase : ")
+print(phr.title)
+print(phr.replace("a","g"),phr.replace("e","g"),phr.replace("i","g"),phr.replace("o","g"),phr.replace("u","g"))
+
+
+
+
+
+
+
+
+
